7dbcac910e9b8d62faba3d19bb57b4a757abbd3b
Владислав <34152979+webproject67@users.noreply.github.com>
2021-11-25 11:19:15 +0300

    panel to plugin

--- src/scripts/slider-jquery.ts
+++ src/scripts/slider-jquery.ts
@@ -1,47 +1,122 @@
-import App from './components/app/App';
+import ConfiguringPanel from './components/configuringPanel/ConfiguringPanel';
 import Presenter from './components/presenter/Presenter';
+import { stateType } from './types';
 
-let app: App;
-let presenter: Presenter;
+export default class Slider {
+  main: HTMLElement;
 
-$.fn.slider = function f(options: object | string, obj?: object) {
-  const state = $.extend(
-    {
-      flag: true,
-      from: -10000,
-      fromPercent: 0,
-      max: 100,
-      min: 0,
-      progress: true,
-      range: 'one',
-      start: 1,
-      scale: true,
-      step: 1,
-      to: -10000,
-      toPercent: 100,
-      view: 'horizontal',
-    },
-    options
-  );
+  state!: stateType;
 
-  if (options === 'getState') return presenter.model.getState();
+  presenter!: Presenter;
 
-  if (options === 'setState') {
-    const keys = Object.keys(obj!);
-    const values = Object.values(obj!);
-    presenter.model.setValue(keys, values);
-    return null;
+  configuringPanel!: ConfiguringPanel;
+
+  constructor(main: HTMLElement, options: object) {
+    this.main = main;
+    this.init(options);
   }
 
-  if (options === 'getConfiguring') {
-    app.renderConfiguringPanel();
-    return null;
+  public init(options: object) {
+    this.state = $.extend(
+      {
+        flag: true,
+        from: -10000,
+        fromPercent: 0,
+        max: 100,
+        min: 0,
+        progress: true,
+        range: 'one',
+        start: 1,
+        scale: true,
+        step: 1,
+        to: -10000,
+        toPercent: 100,
+        view: 'horizontal',
+      },
+      options
+    );
+
+    this.presenter = new Presenter(this.main, this.state);
+
+    return this.presenter;
   }
 
-  const cb = () => {
-    app = new App(this[0], state);
-    app.render();
-  };
+  public getState() {
+    return this.state;
+  }
 
-  return this.each(cb);
+  public setState(options: stateType) {
+    const keys = Object.keys(options);
+    const values = Object.values(options);
+    keys.forEach((key, i) => {
+      switch (key) {
+        case 'start':
+          this.state.start = Number(values[i]);
+          break;
+        case 'min':
+          this.state.min = Number(values[i]);
+          break;
+        case 'max':
+          this.state.max = Number(values[i]);
+          break;
+        case 'step':
+          this.state.step = Number(values[i]);
+          break;
+        case 'from':
+          this.state.from = Number(values[i]);
+          break;
+        case 'fromPercent':
+          this.state.fromPercent = Number(values[i]);
+          break;
+        case 'to':
+          this.state.to = Number(values[i]);
+          break;
+        case 'toPercent':
+          this.state.toPercent = Number(values[i]);
+          break;
+        case 'view':
+          this.state.view = String(values[i]);
+          break;
+        case 'range':
+          this.state.range = String(values[i]);
+          break;
+        case 'flag':
+          this.state.flag = Boolean(values[i]);
+          break;
+        case 'progress':
+          this.state.progress = Boolean(options[key]);
+          break;
+        case 'scale':
+          this.state.scale = Boolean(options[key]);
+          break;
+        default:
+          break;
+      }
+    });
+
+    this.configuringPanel.updateElement();
+    this.presenter.view.updateView(this.state);
+  }
+
+  public showConfiguringPanel(): void {
+    this.configuringPanel = new ConfiguringPanel(this.state, this.main);
+    this.main
+      .querySelector('.slider__wrapper')!
+      .appendChild(this.configuringPanel.getElement());
+
+    this.subscribe();
+  }
+
+  private subscribe() {
+    const cbPanel = (keys: string[], values: (number | string | boolean)[]) =>
+      this.presenter.model.setValue(keys, values);
+    this.configuringPanel.subscribe(cbPanel);
+
+    const cbModel = () => this.configuringPanel.updateElement();
+    this.presenter.model.subscribe(cbModel);
+  }
+}
+
+$.fn.slider = function f(options: object) {
+  return new Slider(this[0], options);
 };
