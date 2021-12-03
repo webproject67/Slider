dc7545fc0b663f15c05245d6a21703e3b5509f61
Владислав <34152979+webproject67@users.noreply.github.com>
2021-12-03 18:30:05 +0300

    jquery declare

--- src/scripts/panel-jquery.ts
+++ src/scripts/panel-jquery.ts
@@ -1,6 +1,12 @@
 import Slider from './Slider';
 import Panel from './Panel';
 
+declare global {
+  interface JQuery {
+    panel(slider: Slider): Panel;
+  }
+}
+
 $.fn.panel = function f(slider: Slider) {
   return new Panel(this[0], slider);
 };
--- src/scripts/slider-jquery.ts
+++ src/scripts/slider-jquery.ts
@@ -1,6 +1,12 @@
 import Slider from './Slider';
 import { IState } from './types';
 
+declare global {
+  interface JQuery {
+    slider(options: IState): Slider;
+  }
+}
+
 $.fn.slider = function f(options: IState) {
   return new Slider(this[0], options);
 };
